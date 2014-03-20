d7bc7db209c37397485c59f99c7bb7e14dd60b66
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-03-20 09:48:38 +0000

    initial commit + constants file

--- YNotToday/Constants.h
+++ YNotToday/Constants.h
@@ -1,13 +1,11 @@
 //
-//  ViewController.h
+//  Constants.h
 //  YNotToday
 //
 //  Created by Stoica Alexandru on 3/20/14.
 //  Copyright (c) 2014 Stoica Alexandru. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+extern NSString *const BaseURL ;
 
-@interface ViewController : UIViewController
-
-@end
---
+++ YNotToday/Constants.m
@@ -0,0 +1,10 @@
+//
+//  Constants.m
+//  YNotToday
+//
+//  Created by Stoica Alexandru on 3/20/14.
+//  Copyright (c) 2014 Stoica Alexandru. All rights reserved.
+//
+
+#import "Constants.h"
+NSString *const BaseURL = @"http://localhost:3000" ;
