949907d7600c6e018453807be2c67f175e1c05bf
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-04-11 17:15:14 +0300

    integrated the contact selection with the next view.

---
+++ YNotToday/QuestionCreationViewController.h
@@ -0,0 +1,17 @@
+//
+//  QuestionCreationViewController.h
+//  YNotToday
+//
+//  Created by Stoica Alexandru on 4/11/14.
+//  Copyright (c) 2014 Stoica Alexandru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Contact;
+
+@interface QuestionCreationViewController : UIViewController
+
+@property (strong, nonatomic) Contact * receiver ;
+
+@end
